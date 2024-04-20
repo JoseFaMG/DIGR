6d18abdc39827bbc9554cda9fae9acc265f7ea51
JoseFaMG <j.muxozcano.g656@edu.utc>
2024-04-19 19:56:56 -0600

    update

--- secundarypages/js/script.js
+++ secundarypages/js/script.js
@@ -86,7 +86,7 @@
     let slideIndex = 0;
     carousel();
 
-    function carousel() {Tecnologías web
+    function carousel() {
         let i;
         const slides = document.querySelectorAll('.card');
         for (i = 0; i < slides.length; i++) {
