fab08a3e599ff49227694daf3575c770ed153ea5
JoseFaMG <j.muxozcano.g656@edu.utc>
2024-04-25 18:41:18 -0600

    update

--- secundarypages/js/script.js
+++ secundarypages/js/script.js
@@ -6,26 +6,3 @@
         tarjeta.classList.toggle('expandida');
     });
 });
-/*mostarmensajedefiniciones*/
-    document.addEventListener('DOMContentLoaded', function() {
-        const tooltips = document.querySelectorAll('.tooltip');
-
-        tooltips.forEach(function(tooltip) {
-            tooltip.addEventListener('click', function(event) {
-                event.stopPropagation();
-                const definition = tooltip.getAttribute('data-definition');
-                const tooltipDefinition = document.createElement('div');
-                tooltipDefinition.classList.add('tooltip-definition');
-                tooltipDefinition.textContent = definition;
-                tooltip.appendChild(tooltipDefinition);
-                tooltipDefinition.classList.toggle('active');
-            });
-        });
-
-        document.addEventListener('click', function() {
-            const activeTooltips = document.querySelectorAll('.tooltip .tooltip-definition.active');
-            activeTooltips.forEach(function(activeTooltip) {
-                activeTooltip.classList.remove('active');
-            });
-        });
-    });
